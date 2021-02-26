e5297dc0cd7510dacc283e13fa365dba3207f5a1
DonMojo <donovan.excels@gmail.com>
2021-02-26 00:49:07 -0500

    Added cart aciton and reducer for cart toggle capability

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,11 @@ import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import {auth} from '../../firebase/firebase.utils';// Lets us deal with firebase database 
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <img className='logo' src={logo} alt="logo" />
@@ -19,15 +21,20 @@ const Header = ({currentUser}) => (
             </Link>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
-                <Link className='option' to='/signin'> SIGN IN</Link>
+                (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>) :
+                (<Link className='option' to='/signin'> SIGN IN</Link>)
             }
-        </div>
+            <CartIcon />
+        </div>{
+            hidden ? null : <CartDropdown />
+        }
+     
     </div>
 )
 
-const mapStateToProps = state => ({ 
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden} }) => ({ 
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)(Header);
