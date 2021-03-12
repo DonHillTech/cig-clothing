541533c2c6ec20f73f52a7e7c96f54984199ba6b
DonMojo <donovan.excels@gmail.com>
2021-03-12 00:21:06 -0500

    Changed shop data and modified selectors

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,13 +1,29 @@
+const  checkIfItemExist = (cartItems, cartItemToFind) => (cartItems.find(
+    cartItem => cartItem.id === cartItemToFind.id
+)
+);
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-        cartItem => cartItem.id == cartItemToAdd.id
-    );
-    if (existingCartItem) {
+   
+    if (checkIfItemExist(cartItems, cartItemToAdd)) {
         return cartItems.map(cartItem =>
                 cartItem.id == cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity+1} : cartItem
                 )
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
+
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem =  checkIfItemExist(cartItems, cartItemToRemove);
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+    )
+
 
 }
